88605206203cbb1aaa29b3ac07e095874c1c7411
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-09-13 15:44:12 +0400

    created components for main pages

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,13 +1,15 @@
 import { RouteObject } from "react-router-dom"
+import Home from "../pages/Home/Home"
+import Favourite from "../pages/Favourite/Favourite"
 
 const HOME: RouteObject = {
     path: "/",
-    element: <>home</>,
+    element: <Home/>,
 }
 
 const FAVOURITE: RouteObject = {
     path: "/favourite",
-    element: <>favourite</>,
+    element: <Favourite/>,
 }
 
 const ROUTES: RouteObject[] = [HOME, FAVOURITE]
