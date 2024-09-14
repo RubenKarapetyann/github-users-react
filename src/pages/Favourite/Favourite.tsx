1e59a5e6a01a00891e2155068547baefea7c47e6
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-09-14 18:41:19 +0400

    implemented search in favourite page

--- src/pages/Favourite/Favourite.tsx
+++ src/pages/Favourite/Favourite.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import { UsersList } from "../../components/Lists";
 import { useFavourite } from "../../contexts/FavouriteContext/FavouriteContext";
 import * as styles from "../../styles/styles.scss"
+import { UserOfList } from "../../types/api/users";
+import { useSearch } from "../../contexts/SearchContext/SearchContext";
 
 export default function Favourite() {
+    const [users, setUsers] = useState<UserOfList[]>([])
     const favourite = useFavourite()
+    const search = useSearch()
 
-    if (!favourite) return
-    const { users } = favourite
+    useEffect(() => {
+        if (!favourite || !search) return
+        const { users } = favourite
 
+        if (!search.value){
+            return setUsers(users)
+        }
+        setUsers(users.filter(user => new RegExp(search.value).test(user.login)))
+    }, [search])
+
+    
     return <div className={styles.container}><UsersList users={users}/></div>
 }
