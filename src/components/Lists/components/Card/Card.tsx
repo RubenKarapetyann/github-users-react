55b48862a8ef5e5eab1c844fe30e6e88ada0b644
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-09-14 12:00:46 +0400

    created component Card with its styles

---
+++ src/components/Lists/components/Card/Card.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import { CardProps } from "../../../../types/components/lists"
+import * as styles from "./Card.module.scss"
+
+const Card = ({ login, avatar_url, id }: CardProps) => (
+    <div className={`${styles.card} ${styles.cardInfo}`}>
+        <div className={styles.cardInfoContainer}>
+            <Link to={`/users/${login}`}>
+                <img src={avatar_url} alt="avatar image" className={styles.avatar}/>
+                <p className={styles.login}>{login}</p>
+                <span className={styles.id}>id: {id}</span>
+            </Link>
+        </div>
+    </div>
+)
+
+export default Card
